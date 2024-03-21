460c2dd7e062b59c42d72b86011deedc0589683b
ramuracozy <rakarabbani18@gmail.com>
2024-03-21 23:00:08 +0700

    push praktikum

--- Praktikum_4/Tugas/index.php
+++ Praktikum_4/Tugas/index.php
@@ -14,7 +14,7 @@ include_once 'sidebar.php';
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item"><a href="#">Layout</a></li>
-              <li class="breadcrumb-item active">Fixed Layout</li>
+              <li class="breadcrumb-item active">Beranda</li>
             </ol>
           </div>
         </div>
